03effb3bc43384d3b2310330c8a7aea0d9baa125
Amit Kumar <amit11jangir@gmail.com>
2025-09-14 21:16:16 +0530

    Update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -10,6 +10,9 @@ export default {
         customdark: "#212121",
         customdarkq: "#313131ff", // the color from your screenshot
       },
+      fontFamily: {
+        syne: ["Syne", "sans-serif"],
+      },
     },
   },
   plugins: [],
